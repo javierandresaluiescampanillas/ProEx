58ebc74075cc2411b0f16e06c81c71f80083736b
Javier <72435329+javierandresaluiescampanillas@users.noreply.github.com>
2020-11-11 11:33:29 +0100

    Add files via upload

---
+++ Examenes/Prueba/Ex01jad/Ex01jad1.java
@@ -0,0 +1,665 @@
+public class Ex01jad1 {
+	
+ public static void main(String[] args) {
+	int iva= ((totalSerigrafia + totalTextil)*21)/100;
+	int polos = 0;
+	System.out.println("Introduzca la cantidad de polos que se desea (como mínimo 20 y como máximo 100):");
+	while (polos <=19 || polos >100) {
+	polos = Integer.parseInt(System.console().readLine());
+	System.out.println("Rcuerde: como mínimo 20 y como máximo 100):");
+	}
+	System.out.print("¿Blanco o a color? (b/c): ");
+    int blancoColor = (s.nextLine()).equals("b");
+	System.out.print("¿Desea serigrafía? (s/n)");
+	int serigrafiaNo = (s.nextLine()).equals("s");
+	System.out.print("¿Desea bordado? (s/n)");
+	int bordadoNo = (s.nextLine()).equals("s");
+	
+	String polos;
+	
+	swich (polos); {
+		case 1:
+			precioUnitario = 4.90;
+			break;		
+		case 2:
+			precioUnitario = 4.90;
+			break;
+		case 3:
+			precioUnitario = 4.90;
+			break;
+		case 4:
+			precioUnitario = 4.90;
+			break;
+		case 5:
+			precioUnitario = 4.90;
+			break;
+		case 6:
+			precioUnitario = 4.90;
+			break;
+		case 7:
+			precioUnitario = 4.90;
+			break;
+		case 8:
+			precioUnitario = 4.90;
+			break;
+		case 9:
+			precioUnitario = 4.90;
+			break;
+		case 10:
+			precioUnitario = 4.90;
+			break;
+		case 11:
+			precioUnitario = 4.90;
+			break;
+		case 12:
+			precioUnitario = 4.90;
+			break;
+		case 13:
+			precioUnitario = 4.90;
+			break;
+		case 14:
+			precioUnitario = 4.90;
+			break;
+		case 15:
+			precioUnitario = 4.90;
+			break;
+		case 16:
+			precioUnitario = 4.90;
+			break;
+		case 17:
+			precioUnitario = 4.90;
+			break;
+		case 18:
+			precioUnitario = 4.90;
+			break;
+		case 19:
+			precioUnitario = 4.90;
+			break;
+		case 20:
+			precioUnitario = 4.90;
+			break;
+		case 21:
+			precioUnitario = 4.90;
+			break;
+		case 22:
+			precioUnitario = 4.90;
+			break;
+		case 23:
+			precioUnitario = 4.90;
+			break;
+		case 24:
+			precioUnitario = 4.90;
+			break;
+		case 25:
+			precioUnitario = 4.90;
+			break;
+		case 26:
+			precioUnitario = 4.90;
+			break;
+		case 27:
+			precioUnitario = 4.90;
+			break;
+		case 28:
+			precioUnitario = 4.90;
+			break;
+		case 29:
+			precioUnitario = 4.90;
+			break;
+		case 30:
+			precioUnitario = 4.90;
+			break;
+		case 31:
+			precioUnitario = 4.90;
+			break;		
+		case 32:
+			precioUnitario = 4.90;
+			break;
+		case 33:
+			precioUnitario = 4.90;
+			break;
+		case 34:
+			precioUnitario = 4.90;
+			break;
+		case 35:
+			precioUnitario = 4.90;
+			break;
+		case 36:
+			precioUnitario = 4.90;
+			break;
+		case 37:
+			precioUnitario = 4.90;
+			break;
+		case 38:
+			precioUnitario = 4.90;
+			break;
+		case 39:
+			precioUnitario = 4.90;
+			break;		
+		case 40:
+			precioUnitario = 3.90;
+			break;
+		case 41:
+			precioUnitario = 3.90;
+			break;
+		case 42:
+			precioUnitario = 3.90;
+			break;
+		case 43:
+			precioUnitario = 3.90;
+			break;
+		case 44:
+			precioUnitario = 3.90;
+			break;
+		case 45:
+			precioUnitario = 3.90;
+			break;
+		case 46:
+			precioUnitario = 3.90;
+			break;
+		case 47:
+			precioUnitario = 3.90;
+			break;
+		case 48:
+			precioUnitario = 3.90;
+			break;
+		case 49:
+			precioUnitario = 3.90;
+			break;
+		case 50:
+			precioUnitario = 3.90;
+			break;
+		case 51:
+			precioUnitario = 3.90;
+			break;
+		case 52:
+			precioUnitario = 3.90;
+			break;
+		case 53:
+			precioUnitario = 3.90;
+			break;
+		case 54:
+			precioUnitario = 3.90;
+			break;
+		case 55:
+			precioUnitario = 3.90;
+			break;
+		case 56:
+			precioUnitario = 3.90;
+			break;
+		case 57:
+			precioUnitario = 3.90;
+			break;
+		case 58:
+			precioUnitario = 3.90;
+			break;
+		case 59:
+			precioUnitario = 3.90;
+			break;
+		case 60:
+			precioUnitario = 3.90;
+			break;
+		case 61:
+			precioUnitario = 3.90;
+			break;
+		case 62:
+			precioUnitario = 3.90;
+			break;
+		case 63:
+			precioUnitario = 3.90;
+			break;
+		case 64:
+			precioUnitario = 3.90;
+			break;
+		case 65:
+			precioUnitario = 3.90;
+			break;
+		case 66:
+			precioUnitario = 3.90;
+			break;
+		case 67:
+			precioUnitario = 3.90;
+			break;
+		case 68:
+			precioUnitario = 3.90;
+			break;
+		case 69:
+			precioUnitario = 3.90;
+			break;
+		case 70:
+			precioUnitario = 3.90;
+			break;
+		case 71:
+			precioUnitario = 3.90;
+			break;
+		case 72:
+			precioUnitario = 3.90;
+			break;
+		case 73:
+			precioUnitario = 3.90;
+			break;
+		case 74:
+			precioUnitario = 3.90;
+			break;
+		case 75:
+			precioUnitario = 3.90;
+			break;
+		case 76:
+			precioUnitario = 3.90;
+			break;
+		case 77:
+			precioUnitario = 3.90;
+			break;
+		case 78:
+			precioUnitario = 3.90;
+			break;
+		case 79:
+			precioUnitario = 3.90;
+			break;
+		case 80:
+			precioUnitario = 3.90;
+			break;
+		case 81:
+			precioUnitario = 3.90;
+			break;
+		case 82:
+			precioUnitario = 3.90;
+			break;
+		case 83:
+			precioUnitario = 3.90;
+			break;
+		case 84:
+			precioUnitario = 3.90;
+			break;
+		case 85:
+			precioUnitario = 3.90;
+			break;
+		case 86:
+			precioUnitario = 3.90;
+			break;
+		case 87:
+			precioUnitario = 3.90;
+			break;
+		case 88:
+			precioUnitario = 3.90;
+			break;
+		case 89:
+			precioUnitario = 3.90;
+			break;
+		case 90:
+			precioUnitario = 3.90;
+			break;
+		case 91:
+			precioUnitario = 3.90;
+			break;
+		case 92:
+			precioUnitario = 3.90;
+			break;
+		case 93:
+			precioUnitario = 3.90;
+			break;
+		case 94:
+			precioUnitario = 3.90;
+			break;
+		case 95:
+			precioUnitario = 3.90;
+			break;
+		case 96:
+			precioUnitario = 3.90;
+			break;
+		case 97:
+			precioUnitario = 3.90;
+			break;
+		case 98:
+			precioUnitario = 3.90;
+			break;
+		case 99:
+			precioUnitario = 3.90;
+			break;
+		case 100:
+			precioUnitario = 3.20;
+			break;
+		default:
+			precioUnitario = 0;
+	
+	
+    String polos;
+	
+	swich(polos); {
+		case 1:
+			serigrafia = 2;
+			break;
+		case 2:
+			serigrafia = 2;
+			break;
+		case 3:
+			serigrafia = 2;
+			break;
+		case 4:
+			serigrafia = 2;
+			break;
+		case 5:
+			serigrafia = 2;
+			break;
+		case 6:
+			serigrafia = 2;
+			break;
+		case 7:
+			serigrafia = 2;
+			break;
+		case 8:
+			serigrafia = 2;
+			break;
+		case 9:
+			serigrafia = 2;
+			break;
+		case 10:
+			serigrafia = 2;
+			break;
+		case 11:
+			serigrafia = 2;
+			break;
+		case 12:
+			serigrafia = 2;
+			break;
+		case 13:
+			serigrafia = 2;
+			break;
+		case 14:
+			serigrafia = 2;
+			break;
+		case 15:
+			serigrafia = 2;
+			break;
+		case 16:
+			serigrafia = 2;
+			break;
+		case 17:
+			serigrafia = 2;
+			break;
+		case 18:
+			serigrafia = 2;
+			break;
+		case 19:
+			serigrafia = 2;
+			break;
+		case 20:
+			serigrafia = 2;
+			break;
+		case 21:
+			serigrafia = 2;
+			break;
+		case 22:
+			serigrafia = 2;
+			break;
+		case 23:
+			serigrafia = 2;
+			break;
+		case 24:
+			serigrafia = 2;
+			break;
+		case 25:
+			serigrafia = 2;
+			break;
+		case 26:
+			serigrafia = 2;
+			break;
+		case 27:
+			serigrafia = 2;
+			break;
+		case 28:
+			serigrafia = 2;
+			break;
+		case 29:
+			serigrafia = 2;
+			break;
+		case 30:
+			serigrafia = 2;
+			break;
+		case 31:
+			serigrafia = 2;
+			break;
+		case 32:
+			serigrafia = 2;
+			break;
+		case 33:
+			serigrafia = 2;
+			break;
+		case 34:
+			serigrafia = 2;
+			break;
+		case 35:
+			serigrafia = 2;
+			break;
+		case 36:
+			serigrafia = 2;
+			break;
+		case 37:
+			serigrafia = 2;
+			break;
+		case 38:
+			serigrafia = 2;
+			break;
+		case 39:
+			serigrafia = 2;
+			break;
+		case 40:
+			serigrafia = 2;
+			break;
+		case 41:
+			serigrafia = 2;
+			break;
+		case 42:
+			serigrafia = 2;
+			break;
+		case 43:
+			serigrafia = 2;
+			break;
+		case 44:
+			serigrafia = 2;
+			break;
+		case 45:
+			serigrafia = 2;
+			break;
+		case 46:
+			serigrafia = 2;
+			break;
+		case 47:
+			serigrafia = 2;
+			break;
+		case 48:
+			serigrafia = 2;
+			break;
+		case 49:
+			serigrafia = 2;
+			break;
+		case 50:
+			serigrafia = 1.50;
+			break;
+		case 51:
+			serigrafia = 1.50;
+			break;
+		case 52:
+			serigrafia = 1.50;
+			break;
+		case 53:
+			serigrafia = 1.50;
+			break;
+		case 54:
+			serigrafia = 1.50;
+			break;
+		case 55:
+			serigrafia = 1.50;
+			break;
+		case 56:
+			serigrafia = 1.50;
+			break;
+		case 57:
+			serigrafia = 1.50;
+			break;
+		case 58:
+			serigrafia = 1.50;
+			break;
+		case 59:
+			serigrafia = 1.50;
+			break;
+		case 60:
+			serigrafia = 1.50;
+			break;
+		case 61:
+			serigrafia = 1.50;
+			break;
+		case 62:
+			serigrafia = 1.50;
+			break;
+		case 63:
+			serigrafia = 1.50;
+			break;
+		case 64:
+			serigrafia = 1.50;
+			break;
+		case 65:
+			serigrafia = 1.50;
+			break;
+		case 66:
+			serigrafia = 1.50;
+			break;
+		case 67:
+			serigrafia = 1.50;
+			break;
+		case 68:
+			serigrafia = 1.50;
+			break;
+		case 69:
+			serigrafia = 1.50;
+			break;
+		case 70:
+			serigrafia = 1.50;
+			break;
+		case 71:
+			serigrafia = 1.50;
+			break;
+		case 72:
+			serigrafia = 1.50;
+			break;
+		case 73:
+			serigrafia = 1.50;
+			break;
+		case 74:
+			serigrafia = 1.50;
+			break;
+		case 75:
+			serigrafia = 1.50;
+			break;
+		case 76:
+			serigrafia = 1.50;
+			break;
+		case 77:
+			serigrafia = 1.50;
+			break;
+		case 78:
+			serigrafia = 1.50;
+			break;
+		case 79:
+			serigrafia = 1.50;
+			break;
+		case 80:
+			serigrafia = 1.50;
+			break;
+		case 81:
+			serigrafia = 1.50;
+			break;
+		case 82:
+			serigrafia = 1.50;
+			break;
+		case 83:
+			serigrafia = 1.50;
+			break;
+		case 84:
+			serigrafia = 1.50;
+			break;
+		case 85:
+			serigrafia = 1.50;
+			break;
+		case 86:
+			serigrafia = 1.50;
+			break;
+		case 87:
+			serigrafia = 1.50;
+			break;
+		case 88:
+			serigrafia = 1.50;
+			break;
+		case 89:
+			serigrafia = 1.50;
+			break;
+		case 90:
+			serigrafia = 1.50;
+			break;
+		case 91:
+			serigrafia = 1.50;
+			break;
+		case 92:
+			serigrafia = 1.50;
+			break;
+		case 93:
+			serigrafia = 1.50;
+			break;
+		case 94:
+			serigrafia = 1.50;
+			break;
+		case 95:
+			serigrafia = 1.50;
+			break;
+		case 96:
+			serigrafia = 1.50;
+			break;
+		case 97:
+			serigrafia = 1.50;
+			break;
+		case 98:
+			serigrafia = 1.50;
+			break;
+		case 99:
+			serigrafia = 1.50;
+			break;
+		case 100:
+			serigrafia = 1.50;
+			break;
+		default:
+			serigrafia = 0;
+	}
+	
+	System.out.print("¿Blanco o a color? (b/c): ");
+    int blancoColor = (s.nextLine()).equals("b");
+	System.out.print("¿Desea serigrafía? (s/n)");
+	int serigrafiaNo = (s.nextLine()).equals("s");
+	System.out.print("¿Desea bordado? (s/n)");
+	int bordadoNo = (s.nextLine()).equals("s");
+	
+	}
+	
+	if ("n".equals (serigrafiaNo)) {
+
+		serigrafia = 0;
+	}
+	if ("b".equals( blancoColor)) {
+		precioUnitario = precioUnitario;
+    } else {
+		precioUnitario = precioUnitario + (((precioUnitario)*21)/100);
+	}
+	
+	int totalTextil = polos * precioUnitario ;
+	int totalSerigrafia = serigrafia * polos ;
+	
+	System.out.println("EL POLITO BONITO - PRESUPUESTO");
+	System.out.println("");
+	System.out.println("Polo en color (unid.)   %2d\n", polos);
+	System.out.println("Precio unitario         %3.2f €\n", precioUnitario);
+	System.out.println("Total textil            %5.2f €\n", totalTextil);
+	System.out.println("Serigrafía (c.u.)       %3.2f €\n", serigrafia);
+	System.out.println("Total serigrafía        %4.2f €\n", totalSerigrafia);
+	System.out.println("Polo mas logotipo       %5.2f €\n", totalTextil + totalSerigrafia);
+	System.out.println("IVA                     %4.2f €\n", iva);
+	System.out.println("TOTAL                   %5.2f €\n", totalTextil + totalSerigrafia + iva);
+  }	
+}
